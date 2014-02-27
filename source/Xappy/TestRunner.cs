6f05fdf4a0e7d61a97cd14d2554f984c9c57028c
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2014-02-27 10:38:32 +0200

    * run tests via intermediatary batch & output collector files   - at least I can hide the command prompt windows this way     and avoid windows' terrible stderr/stdout redirection

--- source/Xappy/TestRunner.cs
+++ source/Xappy/TestRunner.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Xappy
 {
@@ -14,7 +16,7 @@ namespace Xappy
             _vsTestPath = vsTestPath;
         }
 
-        public string ProcessBuild(string parameters)
+        public string ProcessBuild_StreamsConnected(string parameters)
         {
             var proc = new Process();
             var info = new ProcessStartInfo();
@@ -41,6 +43,96 @@ namespace Xappy
                 LogDebug(String.Format("    {0}", line.Trim()));
             LogDebug(" => end of process output");
             return result;
+        }
+
+        public string ProcessBuild(string parameters)
+        {
+            return ProcessBuild_ViaIntermediateFile(parameters);
+        }
+
+        public string ProcessBuild_ViaIntermediateFile(string parameters)
+        {
+            if (!File.Exists(_vsTestPath))
+            {
+                throw new Exception("Unable to find the vstest binary at the configuration location: '" + _vsTestPath + "'");
+            }
+            try
+            {
+                using (var executor = new ExecutorBatchFileWrapper(_vsTestPath, parameters))
+                {
+                    LogDebug("=> starting test process");
+                    var result = executor.Execute();
+                    LogDebug("=> test process completed");
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return String.Join("\n", new[] { "Unable to run tests:", ex.Message, ex.StackTrace });
+            }
+        }
+
+    }
+
+    public class ExecutorBatchFileWrapper : IDisposable
+    {
+        private readonly string _tempOutputFile;
+        private readonly string _tempBatchFile;
+
+        public ExecutorBatchFileWrapper(string vsTestPath, string parameters)
+        {
+            _tempOutputFile = Path.GetTempFileName();
+            _tempBatchFile = Path.GetTempFileName() + ".bat";
+            var cmd = String.Format("\"{0}\" /InIsolation {1} > \"{2}\" 2>&1", vsTestPath, parameters, _tempOutputFile);
+            File.WriteAllBytes(_tempBatchFile, Encoding.UTF8.GetBytes(cmd));
+        }
+
+        public string Execute()
+        {
+            using (var proc = CreateProcessToRunTemporaryBatchFile())
+            {
+                proc.Start();
+                proc.WaitForExit();
+                var result = String.Join("\n", new[] { "Test run commandline: ", proc.StartInfo.FileName + " " + proc.StartInfo.Arguments, 
+                ReadContentsFromTempOutputFile() });
+                return result;
+            }
+        }
+
+        private Process CreateProcessToRunTemporaryBatchFile()
+        {
+            return new Process()
+                   {
+                       StartInfo = new ProcessStartInfo 
+                                   {
+                                       FileName = _tempBatchFile, 
+                                       RedirectStandardError = false, 
+                                       RedirectStandardInput = false, 
+                                       RedirectStandardOutput = false, 
+                                       UseShellExecute = true, 
+                                       WindowStyle = ProcessWindowStyle.Hidden
+                                   }
+                   };
+        }
+
+        private string ReadContentsFromTempOutputFile()
+        {
+            return Encoding.UTF8.GetString(File.ReadAllBytes(_tempOutputFile));
+        }
+
+        public void Dispose()
+        {
+            lock (this)
+            {
+                foreach (var f in new[] { _tempOutputFile, _tempBatchFile })
+                {
+                    try
+                    {
+                        File.Delete(f);
+                    }
+                    catch { }
+                }
+            }
         }
     }
 }
